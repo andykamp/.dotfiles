355ced401242f11e210f1c164c3ca31f1bd2d14b
andykamp <akampenes@gmail.com>
2023-10-14 10:43:55 +0200

    feat(maps) add <leader>s as signature

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -93,6 +93,7 @@ local on_attach = function(_, bufnr)
 
     -- info
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>s', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>c', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts) -- gives a list of action the IDE can do for you
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so',
         [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts) -- list all variables in float
