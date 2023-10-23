94f68b75cbf7af43a746c157d641234e5f2793ee
andykamp <akampenes@gmail.com>
2023-10-23 19:38:17 +0200

    feat: add <leader>xx for trouble diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,12 +34,10 @@ require('packer').startup(function()
 
     use 'tpope/vim-surround' -- enables the s and S command
 
-    -- Auto pairs
     use {
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
-
 
     use "folke/todo-comments.nvim"
 
@@ -130,16 +128,9 @@ require('packer').startup(function()
     -- use 'mlaursen/vim-react-snippets' -- spesific reat snippets
     use 'github/copilot.vim'
 
-    use { -- @todo: remove
+    use {
         "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
+        requires = "kyazdani42/nvim-web-devicons"
     }
 
     -- Markdown and documentation
@@ -162,6 +153,7 @@ require('maps')
 require('settings')
 require("config.copilot")
 require("config.transparant")
+require("config.trouble")
 require("config.colorizer")
 require("config.gitsigns")
 require("config.fugitive")
