7364377a7a76db6acde0981d01bd8b743b549871
andykamp <akampenes@gmail.com>
2023-10-23 18:40:58 +0200

    fix: update indent-blankline migration to v3

--- nvim/lua/config/indent-blankline.lua
+++ nvim/lua/config/indent-blankline.lua
@@ -1,9 +1,11 @@
-vim.g.indent_blankline_char = '┊'
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
-vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-require("indent_blankline").setup {
-    show_current_context = true,
-    show_current_context_start = true,
+require("ibl").setup {
+    indent = { char = '┊' },
+    whitespace =  {
+       remove_blankline_trail = false,
+    },
+    exclude= {
+        filetypes = { "help", "packer" },
+        buftypes = { "terminal", 'nofile' },
+    },
 }
 
