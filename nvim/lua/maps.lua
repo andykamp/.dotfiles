e5b83872b447f83808c607556b9e261f6503c50c
andykamp <akampenes@gmail.com>
2024-03-07 08:49:55 +0100

    feat: remap s to be search /

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -19,6 +19,9 @@ vim.g.maplocalleader = ' '
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 
+-- replace s with search /
+map('n', 's', '/', options)
+
 -- jk is escape
 map('i', 'jk', '<esc>', options)
 
